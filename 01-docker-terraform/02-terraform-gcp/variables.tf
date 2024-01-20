8648f9225dd23f7b1b40a48f0808b85c9a4931e1
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2024-01-20 15:00:45 +0700

    update variables tf

---
+++ 01-docker-terraform/02-terraform-gcp/variables.tf
@@ -0,0 +1,29 @@
+variable "project" {
+  description = "Project"
+  default = "<PROJECT ID>"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "ASIA-SOUTHEAST2"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "ASIA-SOUTHEAST2"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "PROJECT-ID-terra-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
