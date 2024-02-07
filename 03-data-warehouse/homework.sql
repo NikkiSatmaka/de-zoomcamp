224408acaf90ce8ba03170b5f4efbaafa3297102
Nikki Satmaka <nsatmaka@gmail.com>
2024-02-07 14:09:03 +0700

    update 03: homework

--- 03-data-warehouse/homework.sql
+++ 03-data-warehouse/homework.sql
@@ -17,12 +17,15 @@ PARTITION BY
   DATE(`lpep_pickup_datetime`) AS
 SELECT * FROM `de-zoomcamp-411717.nytaxi.external_green_tripdata`;
 
+-- Query scans 0 B
 SELECT DISTINCT `PULocationID`
 FROM `de-zoomcamp-411717.nytaxi.external_green_tripdata`;
 
+-- Query scans 6.41 MB
 SELECT DISTINCT `PULocationID`
 FROM `de-zoomcamp-411717.nytaxi.green_tripdata_non_partitioned`;
 
+-- Query scans 6.41 MB
 SELECT DISTINCT `PULocationID`
 FROM `de-zoomcamp-411717.nytaxi.green_tripdata_partitioned`;
 
@@ -44,10 +47,20 @@ FROM `de-zoomcamp-411717.nytaxi.INFORMATION_SCHEMA.PARTITIONS`
 ORDER BY table_name, partition_id;
 
 -- Write a query to retrieve the distinct PULocationID between lpep_pickup_datetime 06/01/2022 and 06/30/2022 (inclusive)
+-- Query scans 12.82 MB
 SELECT DISTINCT `PULocationID`
 FROM `de-zoomcamp-411717.nytaxi.green_tripdata_non_partitioned`
 WHERE `lpep_pickup_datetime` BETWEEN '2022-06-01' AND '2022-06-30';
 
+-- Query scans 1.12 MB
 SELECT DISTINCT `PULocationID`
 FROM `de-zoomcamp-411717.nytaxi.green_tripdata_partitioned_clustered`
 WHERE `lpep_pickup_datetime` BETWEEN '2022-06-01' AND '2022-06-30';
+
+-- Query scans 0 B
+SELECT COUNT(*)
+FROM `de-zoomcamp-411717.nytaxi.green_tripdata_partitioned_clustered`;
+
+-- Query scans 0 B
+SELECT COUNT(*)
+FROM `de-zoomcamp-411717.nytaxi.green_tripdata_non_partitioned`;
